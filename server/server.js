f19f9dbc651750f4360eef76171d9ea69ee9a0d9
Harris Gurung <harris@Harriss-Mac-mini.local>
2018-01-15 10:27:56 +0545

    Lodash added and patch method updated

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
@@ -59,6 +60,32 @@ app.delete('/todos/:id', (req, res) => {
     }).catch((err) => {
         res.status(400).send();
     });
+});
+
+app.patch('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    var body = _.pick(req.body, ['text','completed']);
+
+    if(!ObjectID.isValid(id)) {
+        res.status(404).send();
+    }
+
+    if(_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, {$set: body }, {new: true}).then((todo) => {
+        if(!todo) {
+            res.status(404).send();
+        }
+        res.send({todo});
+    }).catch((err) => {
+        res.status(400).send();
+    });
+
 });
 
 app.listen(port, () => {
